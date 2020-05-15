0acde236c5e349e5bb67ebb9a2e116541a36dedb
dohye kim <dohyedohye@gmail.com>
2020-05-16 08:37:14 +0900

    Update numpy_append.py

--- numpy_append.py
+++ numpy_append.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 # numpy에서도 vstack, hstack, dstack, stack이 있지만 append사용이 가능함.
+# 근데 array_ops.append는 작동하지 않았음.
 # 아래 예시는 train_examples에 두개의 리스트가 있고, 각각의 리스트에는 input array, target array가 들어 있음
 # 그때 input따로 붙이고, target따로 붙이고자 아래의 코드를 이용함
 
